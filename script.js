0b8d2108264d17c98b90a15bc1ed2d1c77021ca5
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-02-09 11:26:53 -0300

    update commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#telefone').mask('(00) 00000-0000',{
+        placeholder:'(__) _____-____'
+    })
+
+    $('.menu-hamburger').click(function(){
+        $('.menus').slideToggle();
+        
+        })
+})
+
